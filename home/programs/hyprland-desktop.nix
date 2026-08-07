6bce1997c33ba6bf179f1dcfe744cf97db3ee754
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2026-08-07 16:15:23 +0200

    zen time

--- home/programs/hyprland-desktop.nix
+++ home/programs/hyprland-desktop.nix
@@ -114,7 +114,7 @@
         # Azioni sistema
         "SUPER, RETURN, exec, ghostty"
         "SUPER, N, exec, nautilus"
-        "SUPER, C, exec, brave"
+        "SUPER, C, exec, zen"
         "SUPER SHIFT, Q, killactive,"
         "SUPER SHIFT, C, exec, hyprctl reload"
         "SUPER SHIFT, E, exit,"
